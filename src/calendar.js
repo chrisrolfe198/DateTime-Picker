28062fc1971e68e871d9ff343d12e6ab546cbae4
Christopher Rolfe <christopher.rolfe198@gmail.com>
2015-02-24 12:42:42 +0000

    Adding a basic Calendar class

--- src/calendar.js
+++ src/calendar.js
@@ -8,15 +8,3 @@
 	}
 	exports.Calendar = Calendar;
 })(window);
-(function(exports) {
-	"use strict";
-
-	function DateTimePicker(currentDate) {
-		if (currentDate !== undefined && new Date(currentDate)) {
-			this.currentDate = new Date(currentDate);
-		} else {
-			this.currentDate = new Date();
-		}
-	}
-	exports.DateTimePicker = DateTimePicker;
-})(window);
